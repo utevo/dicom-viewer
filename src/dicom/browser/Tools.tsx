5f50b405e4031c48edc0a75449b2cdb6f13fa941
Michał Kowieski <michael.kowieski@gmail.com>
2021-04-25 22:00:37 +0200

    feat: update a11y

--- src/dicom/browser/Tools.tsx
+++ src/dicom/browser/Tools.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const Tools = ({ tool, onToolChange }: Props): React.ReactElement => {
   return (
-    <select value={tool} onChange={(e) => onToolChange(e.target.value as Tool)}>
+    <select value={tool} onBlur={(e) => onToolChange(e.target.value as Tool)}>
       <option value={Tool.Nothing}>Nothing</option>
       <option value={Tool.Pan}>Pan</option>
       <option value={Tool.Rotate}>Rotate</option>
