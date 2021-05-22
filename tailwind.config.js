213b9da3b5bbc325be14c9c4c235f3ac5486711f
Michał Kowieski <michael.kowieski@gmail.com>
2021-05-22 20:03:54 +0200

    fix: fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
