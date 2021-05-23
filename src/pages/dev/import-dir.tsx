99d3287bb9ecb298f42ff02c065786b68f031b9f
Michał Kowieski <michael.kowieski@gmail.com>
2021-05-23 17:27:19 +0200

    feat: remove dev pages

--- src/pages/dev/import-dir.tsx
+++
@@ -1,7 +0,0 @@
-import { InputDirectory } from "../../dicom/browser/InputDirectory";
-
-const ImportDirPage = (): React.ReactElement => {
-  return <InputDirectory onDirectoryHandleChange={() => ({})} />;
-};
-
-export default ImportDirPage;
